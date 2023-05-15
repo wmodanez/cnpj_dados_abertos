488c0a266e51a0370435006460eaa7236d705232
Wesley Modanez Freitas <wesley.modanez@goias.gov.br>
2023-05-15 15:51:39 -0300

    Inclusão de uma progressbar; Verificação se arquivo já foi baixado e se o mesmo é o mais atual antes de baixar novamente; Refatoramento do código para usar tipificação explicita.

--- dados_cnpj_lista_url.py
+++ dados_cnpj_lista_url.py
@@ -1,107 +1,74 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-lista relação de arquivos na página de dados públicos da receita federal
-"""
-
-url = 'https://www.gov.br/receitafederal/pt-br/assuntos/orientacao-tributaria/cadastros/consultas/dados-publicos-cnpj'
-url = 'http://200.152.38.155/CNPJ/'
-from bs4 import BeautifulSoup, SoupStrainer
+import os
+import time
+import urllib
+import datetime
 import requests
+import progressbar
+from bs4 import BeautifulSoup
+
+URL: str = 'http://200.152.38.155/CNPJ/'
+ZIP_FOLDER: str = 'dados-publicos-zip/'
+
+progress_bar: progressbar = None
 
 
-page = requests.get(url)    
-data = page.text
-soup = BeautifulSoup(data)
+def show_progress(block_num: int, block_size: int, total_size: int):
+    global progress_bar
+
+    if progress_bar is None:
+        progress_bar = progressbar.ProgressBar(maxval=total_size)
+        progress_bar.start()
+
+    downloaded: int = block_num * block_size
+
+    if downloaded < total_size:
+        progress_bar.update(downloaded)
+    else:
+        progress_bar.finish()
+        progress_bar = None
+
+
+def check_file_exists(path: str, filename: str) -> int:
+    if os.path.exists(path + filename):
+        return os.stat(path + filename).st_size
+    return 0
+
+
+soup: BeautifulSoup = BeautifulSoup(requests.get(URL).text)
+
+
+def download_file():
+    urllib.request.urlretrieve(file_url, ZIP_FOLDER + file_download, show_progress)
+    os.utime(ZIP_FOLDER + file_download, (timestamp_last_modified, timestamp_last_modified))
+
 
 for link in soup.find_all('a'):
-    if str(link.get('href')).endswith('.zip'): 
-        cam = link.get('href')
-        # if cam.startswith('http://http'):
-        #     cam = 'http://' + cam[len('http://http//'):] 
-        if not cam.startswith('http'):
-            print(url+cam)
+    if str(link.get('href')).endswith('.zip'):
+        file_download: str = link.get('href')
+        file_url: str = URL + file_download
+
+        if not file_download.startswith('http'):
+            local_file: int = check_file_exists(ZIP_FOLDER, file_download)
+            # print(requests.head(url_file).headers)
+            file_url_last_modified: list = requests.head(file_url).headers['Last-Modified'].split()
+
+            file_url_last_modified_time: str = str(file_url_last_modified[4]).split(':')
+            timestamp_last_modified: int = datetime.datetime(int(file_url_last_modified[3]),
+                                                             int(time.strptime(file_url_last_modified[2], '%b').tm_mon),
+                                                             int(file_url_last_modified[1]),
+                                                             int(file_url_last_modified_time[0]),
+                                                             int(file_url_last_modified_time[1]),
+                                                             int(file_url_last_modified_time[2])).timestamp()
+
+            print('Baixando o arquivo: ' + file_download)
+            if local_file == 0:
+                download_file()
+            elif local_file > 0 and local_file != int(requests.head(file_url).headers['Content-Length']):
+                download_file()
+            else:
+                print('O arquivo', file_download, 'esta atualizado.')
         else:
-            print(cam)
-'''
-http://200.152.38.155/CNPJ/F.K03200$W.SIMPLES.CSV.D10911.zip
-http://200.152.38.155/CNPJ/F.K03200$Z.D10911.CNAECSV.zip
-http://200.152.38.155/CNPJ/F.K03200$Z.D10911.MOTICSV.zip
-http://200.152.38.155/CNPJ/F.K03200$Z.D10911.MUNICCSV.zip
-http://200.152.38.155/CNPJ/F.K03200$Z.D10911.NATJUCSV.zip
-http://200.152.38.155/CNPJ/F.K03200$Z.D10911.PAISCSV.zip
-http://200.152.38.155/CNPJ/F.K03200$Z.D10911.QUALSCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y0.D10911.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y0.D10911.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y0.D10911.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y1.D10911.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y1.D10911.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y1.D10911.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y2.D10911.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y2.D10911.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y2.D10911.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y3.D10911.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y3.D10911.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y3.D10911.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y4.D10911.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y4.D10911.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y4.D10911.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y5.D10911.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y5.D10911.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y5.D10911.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y6.D10911.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y6.D10911.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y6.D10911.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y7.D10911.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y7.D10911.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y7.D10911.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y8.D10911.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y8.D10911.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y8.D10911.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y9.D10911.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y9.D10911.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y9.D10911.SOCIOCSV.zip
+            print('Não foi possível baixar o arquivo: ' + file_download)
 
-
-'''
-        
-'''
-http://200.152.38.155/CNPJ/F.K03200$W.SIMPLES.CSV.D10814.zip
-http://200.152.38.155/CNPJ/F.K03200$Z.D10814.CNAECSV.zip
-http://200.152.38.155/CNPJ/F.K03200$Z.D10814.MOTICSV.zip
-http://200.152.38.155/CNPJ/F.K03200$Z.D10814.MUNICCSV.zip
-http://200.152.38.155/CNPJ/F.K03200$Z.D10814.NATJUCSV.zip
-http://200.152.38.155/CNPJ/F.K03200$Z.D10814.PAISCSV.zip
-http://200.152.38.155/CNPJ/F.K03200$Z.D10814.QUALSCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y0.D10814.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y0.D10814.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y0.D10814.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y1.D10814.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y1.D10814.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y1.D10814.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y2.D10814.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y2.D10814.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y2.D10814.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y3.D10814.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y3.D10814.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y3.D10814.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y4.D10814.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y4.D10814.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y4.D10814.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y5.D10814.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y5.D10814.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y5.D10814.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y6.D10814.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y6.D10814.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y6.D10814.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y7.D10814.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y7.D10814.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y7.D10814.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y8.D10814.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y8.D10814.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y8.D10814.SOCIOCSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y9.D10814.EMPRECSV.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y9.D10814.ESTABELE.zip
-http://200.152.38.155/CNPJ/K3241.K03200Y9.D10814.SOCIOCSV.zip
-'''
+print('Fim!')
