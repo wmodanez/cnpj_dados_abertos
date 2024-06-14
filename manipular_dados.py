d7780311978fadb2851a78fe528a00395a376c49
Wesley Modanez Freitas <wesley.modanez@gmail.com>
2024-06-14 09:17:21 -0300

    Otimização do código e implementação da funcionalidade para criação de arquivo único com todas as tabelas em formato parquet.

--- manipular_dados.py
+++ manipular_dados.py
@@ -1,6 +1,8 @@
+# pip install requests progressbar "dask[complete]"
 import datetime
 import glob
 import os
+import duckdb as duck
 import zipfile
 from multiprocessing import freeze_support
 
@@ -9,7 +11,6 @@ import numpy as np
 from dask.diagnostics import ProgressBar
 from dask.distributed import Client
 from dotenv import load_dotenv
-from sqlalchemy import types
 
 pgBar = ProgressBar()
 pgBar.register()
@@ -18,7 +19,7 @@ pgBar.register()
 YYYYMM = (datetime.date.today()).strftime('%Y%m')
 
 
-def manipular_empresa(db: str, isParquet: bool = False) -> dd:
+def manipular_empresa() -> dd:
     colunas_empresa: list = [
         'cnpj_basico',
         'razao_social',
@@ -49,49 +50,24 @@ def manipular_empresa(db: str, isParquet: bool = False) -> dd:
         ddEmpresa = ddEmpresa.set_index('cnpj_basico')
         print('Tempo de manipulação dos dados:', str(datetime.datetime.now() - interTimer))
 
-        if isParquet:
-            try:
-                createParquet(ddEmpresa, tableName)
-            except Exception as e:
-                print(f'Error: {e}')
+        createParquet(ddEmpresa, tableName)
 
         # Tratamento para criar as empresas privadas
         ddEmpresaPrivada = ddEmpresa[ddEmpresa['natureza_juridica'].between(2046, 2348)]
         ddEmpresaPrivada = ddEmpresaPrivada.drop('qualificacao_responsavel', axis=1)
         ddEmpresaPrivada = ddEmpresaPrivada.drop('ente_federativo_responsavel', axis=1)
 
-        try:
-            empresaTypes = dict(zip(
-                ddEmpresa.columns.tolist(),
-                (types.String(), types.Integer(), types.String(), types.String(), types.Integer(), types.String())))
-            saveInBD(db=db, tableName='empresa', ddTable=ddEmpresa, descMsg='de Empresas', dictTypes=empresaTypes)
-        except Exception as e:
-            print(f'Error: {e}')
-
-        try:
-            empresaPrivadaTypes = dict(zip(
-                ddEmpresaPrivada.columns.tolist(),
-                (types.String(), types.Integer(), types.String(), types.Integer())))
-            saveInBD(db=db, tableName='empresa_privada', ddTable=ddEmpresaPrivada, descMsg='de Empresas Privadas',
-                     dictTypes=empresaPrivadaTypes)
-        except Exception as e:
-            print(f'Error: {e}')
-
     except Exception as e:
         print(f'Error: {e}')
 
-        if isParquet:
-            try:
-                createParquet(ddEmpresaPrivada, 'empresa_privada')
-            except Exception as e:
-                print(f'Error: {e}')
+        createParquet(ddEmpresaPrivada, 'empresa_privada')
 
     finally:
         deleteFile(PATH_UNZIP)
         print('Tempo total de manipulação das Empresas:', str(datetime.datetime.now() - startTime))
 
 
-def manipular_estabelecimento(db: str, isParquet: bool = False):
+def manipular_estabelecimento() -> dd:
     startTime: datetime = datetime.datetime.now()
     colunas_estabelecimento: list = [
         'cnpj_basico',
@@ -193,41 +169,12 @@ def manipular_estabelecimento(db: str, isParquet: bool = False):
         ddEstabelecimento = ddEstabelecimento.set_index('cnpj_basico')
         print('Tempo de manipulação dos dados:', str(datetime.datetime.now() - startTime))
 
-        if isParquet:
-            try:
-                createParquet(ddEstabelecimento, 'estabelecimento')
-            except AssertionError as e:
-                print(f'Error: {e}')
-            except Exception as e:
-                print(f'Error: {e}')
+        createParquet(ddEstabelecimento, 'estabelecimento')
 
-        # try:
-        #     estabelecimentoTypes = dict(zip(
-        #         ddEstabelecimento.columns.tolist(),
-        #         (types.Integer(), types.String(), types.Integer(), types.DATE(), types.Integer(), types.DATE(),
-        #          types.Integer(), types.String(), types.String(), types.String(), types.Integer(), types.BigInteger(),
-        #          types.Integer())))
-        #
-        #     saveInBD(db=db, tableName='estabelecimento', ddTable=ddEstabelecimento, descMsg='de Estabelecimentos',
-        #              dictTypes=estabelecimentoTypes)
-        # except Exception as e:
-        #     print(f'Error: {e}')
+        ddEstabelecimentoGO = ddEstabelecimento[ddEstabelecimento['uf'] == 'GO']
+        createParquet(ddEstabelecimentoGO, 'estabelecimento_go')
 
-        if isParquet:
-            try:
-                ddEstabelecimentoGO = ddEstabelecimento[ddEstabelecimento['uf'] == 'GO']
-                createParquet(ddEstabelecimentoGO, 'estabelecimento_go')
-                del ddEstabelecimentoGO
-            except Exception as e:
-                print(f'Error: {e}')
-
-        # try:
-        #     saveInBD(db=db, tableName='estabelecimento_go', ddTable=ddEstabelecimentoGO,
-        #              descMsg='dos Estabelecimentos de Goiás', dictTypes=estabelecimentoTypes)
-        # except Exception as e:
-        #     print(f'Error: {e}')
-
-        return ddEstabelecimento
+        return ddEstabelecimento, ddEstabelecimentoGO
 
     except Exception as e:
         print(f'Error: {e}')
@@ -236,7 +183,7 @@ def manipular_estabelecimento(db: str, isParquet: bool = False):
         print('Tempo total de manipulação dos Estabelecimentos:', str(datetime.datetime.now() - startTime))
 
 
-def manipular_simples(db: str, isParquet: bool = False) -> dd:
+def manipular_simples() -> dd:
     startTime: datetime = datetime.datetime.now()
     colunas_simples: list = [
         'cnpj_basico',
@@ -278,20 +225,7 @@ def manipular_simples(db: str, isParquet: bool = False) -> dd:
 
         print('Tempo de manipulação dos dados:', str(datetime.datetime.now() - interTimer))
 
-        if isParquet:
-            try:
-                createParquet(ddSimples, 'simples')
-            except Exception as e:
-                print(f'Error: {e}')
-
-        try:
-            simples_types = dict(zip(
-                ddSimples.columns.tolist(),
-                (types.Integer(), types.DATE(), types.DATE(), types.Integer(), types.DATE(), types.DATE())))
-
-            saveInBD(db=db, tableName='simples', ddTable=ddSimples, descMsg='do Simples', dictTypes=simples_types)
-        except Exception as e:
-            print(f'Error: {e}')
+        createParquet(ddSimples, 'simples')
 
         return ddSimples
     except Exception as e:
@@ -301,7 +235,7 @@ def manipular_simples(db: str, isParquet: bool = False) -> dd:
         print('Tempo total de trabalho com o Simples:', str(datetime.datetime.now() - startTime))
 
 
-def manipular_socio(db: str, isParquet: bool = False) -> dd:
+def manipular_socio() -> dd:
     startTime: datetime = datetime.datetime.now()
     colunas_socio: list = [
         'cnpj_basico',
@@ -334,35 +268,20 @@ def manipular_socio(db: str, isParquet: bool = False) -> dd:
         extrairArquivo(PATH_ZIP, PATH_UNZIP, '*Soc*')
 
         print('Início da manipulação dos Sócios')
-        dd_socio = dd.read_csv('dados-abertos/*SOCIO*', sep=';', names=colunas_socio, encoding='latin1',
-                               dtype=dtype_socio, na_filter=None)
+        ddSocio = dd.read_csv('dados-abertos/*SOCIO*', sep=';', names=colunas_socio, encoding='latin1',
+                              dtype=dtype_socio, na_filter=None)
 
         interTimer = datetime.datetime.now()
-        dd_socio['cnpj_basico'] = dd_socio.cnpj_basico.astype('int')
-        dd_socio['identificador_socio'] = dd_socio.identificador_socio.astype('int')
-        dd_socio['qualificacao_socio'] = dd_socio.qualificacao_socio.astype('int')
-        dd_socio['qualificacao_representante_legal'] = dd_socio.qualificacao_representante_legal.astype('int')
-        dd_socio['faixa_etaria'] = dd_socio.faixa_etaria.astype('int')
+        ddSocio['cnpj_basico'] = ddSocio.cnpj_basico.astype('int')
+        ddSocio['identificador_socio'] = ddSocio.identificador_socio.astype('int')
+        ddSocio['qualificacao_socio'] = ddSocio.qualificacao_socio.astype('int')
+        ddSocio['qualificacao_representante_legal'] = ddSocio.qualificacao_representante_legal.astype('int')
+        ddSocio['faixa_etaria'] = ddSocio.faixa_etaria.astype('int')
         print('Tempo de manipulação dos dados:', str(datetime.datetime.now() - interTimer))
 
-        if isParquet:
-            try:
-                createParquet(dd_socio, 'socio')
-            except Exception as e:
-                print(f'Error: {e}')
+        createParquet(ddSocio, 'socio')
 
-        # try:
-        #     interTimer = datetime.datetime.now()
-        #     socio_types = dict(zip(
-        #         dd_socio.columns.tolist(),
-        #         (types.Integer(), types.Integer(), types.String(), types.String(), types.Integer(), types.DATE(),
-        #          types.String(), types.String(), types.String(), types.Integer(), types.Integer())))
-        #     saveInBD(db=db, tableName='socio', ddTable=dd_socio, descMsg='dos Sócios', dictTypes=socio_types)
-        # except Exception as e:
-        #     print(f'Error: {e}')
-        # finally:
-        #     print('Tempo de criação dos arquivos parquet:', str(datetime.datetime.now() - interTimer))
-
+        return ddSocio
     except Exception as e:
         print(f'Error: {e}')
     finally:
@@ -376,7 +295,7 @@ def createParquet(daskDataframe: dd, tableName: str):
         daskDataframe.to_parquet(PATH_PARQUET + '\\' + YYYYMM + '\\' + tableName, engine='pyarrow',
                                  write_metadata_file=True, overwrite=True,
                                  name_function=lambda x: f'{YYYYMM}_{tableName}{x}.parquet')
-        print('Tempo de criação dos arquivos parquet:', str(datetime.datetime.now() - interTimer))
+        print(f'Tempo de criação dos arquivos parquet ({tableName}):', str(datetime.datetime.now() - interTimer))
     except Exception as e:
         raise
 
@@ -395,13 +314,30 @@ def deleteFile(folderDelete: str, fileName: str = '*'):
         os.remove(arq)
 
 
-def saveInBD(db: str, tableName: str, descMsg: str, ddTable: dd, dictTypes: dict):
-    interTimer = datetime.datetime.now()
-    print(f'Iniciando a inclusão no banco {ddTable}')
+def create_db_parquet():
+    print('Início da criação das tabelas parquet')
+    startTime: datetime = datetime.datetime.now()
+    conn = duck.connect(f'parquet/cnpj.duckdb', config={'threads': 4})
 
-    ddTable.to_sql(tableName, uri=db, index=True, if_exists='replace', parallel=True, chunksize=1000, method='multi',
-                   dtype=dictTypes)
-    print('Tempo de insert na tabela', descMsg, str(datetime.datetime.now() - interTimer))
+    listTabela = ['empresa', 'estabelecimento', 'estabelecimento_go', 'simples', 'socio']
+    for tabela in listTabela:
+        path_parquet = PATH_PARQUET + '/' + YYYYMM + '/' + tabela + '/*.parquet'
+        add_table_parquet(conn, tabela, path_parquet)
+    listTabelaFixa = ['cnae', 'motivo', 'municipio']
+    for tabela in listTabelaFixa:
+        path_parquet = PATH_PARQUET + '/base/' + tabela + '.parquet'
+        add_table_parquet(conn, tabela, path_parquet)
+    print('Tempo total de carga das tabelas:', str(datetime.datetime.now() - startTime))
+
+
+def add_table_parquet(conn: duck.connect(), table: str, pathParquet: str):
+    interTimer = datetime.datetime.now()
+    sql = f'''
+        CREATE OR REPLACE TABLE {table} AS 
+            (SELECT * FROM '{pathParquet}')
+    '''
+    conn.execute(sql)
+    print(f'Tempo de carregamento da tabela {table}:', str(datetime.datetime.now() - interTimer))
 
 
 if __name__ == '__main__':
@@ -413,20 +349,16 @@ if __name__ == '__main__':
     elif env == 'prod':
         load_dotenv('.env')
 
-    DB_NAME: str = os.getenv('DB_NAME')
-    USERNAME: str = os.getenv('DB_USERNAME')
-    PASSWORD: str = os.getenv('DB_PASSWORD')
-    HOST: str = os.getenv('DB_HOST')
     PATH_ZIP: str = os.getenv('PATH_ZIP')
     PATH_UNZIP: str = os.getenv('PATH_UNZIP')
     PATH_PARQUET: str = os.getenv('PATH_PARQUET')
 
-    db = f'postgresql+psycopg2://{USERNAME}:{PASSWORD}@{HOST}/{DB_NAME}'
-
     freeze_support()
     client = Client()
     print(client)
-    manipular_empresa(db, isParquet=True)
-    manipular_estabelecimento(db, isParquet=True)
-    manipular_simples(db, isParquet=True)
-    manipular_socio(db, isParquet=True)
+
+    manipular_empresa()
+    manipular_estabelecimento()
+    manipular_simples()
+    manipular_socio()
+    create_db_parquet()
